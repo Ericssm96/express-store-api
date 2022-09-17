ce60a97b4039b07958c316fe107ad7036aa3d96a
Eric Stefano Silva Milhomem <94946025+Ericssm96@users.noreply.github.com>
2022-09-17 16:24:15 -0300

    Search for specific fields (product attributes) allowed

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,12 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
   const search = "alb";
-  const products = await Product.find({}).sort('-name -price');
+  const products = await Product.find({}).select('name price');
   res.status(200).json({products, nbHits: products.length});
 }
 
 const getAllProducts = async (req, res) => {
-  const {featured, company, name, sort} = req.query;
+  const {featured, company, name, sort, fields} = req.query;
   const queryObject = {};
 
   if(featured){
@@ -24,11 +24,17 @@ const getAllProducts = async (req, res) => {
 
   let result = Product.find(queryObject);
 
+  // sorting
   if(sort) {
     const sortList = sort.split(/[,.\s]/).join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
+  }
+
+  if(fields) {
+    const fieldsList = fields.split(/[,.\s]/).join(' ');
+    result = result.select(fieldsList);
   }
 
   let products = await result;
