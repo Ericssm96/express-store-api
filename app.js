4b6b5a517768fb71a6af2293d6f10fa876f8d2ec
Eric Stefano Silva Milhomem <94946025+Ericssm96@users.noreply.github.com>
2022-09-16 22:11:21 -0300

    Created Schema

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
-// async errors
+require('express-async-errors');
 const express = require('express');
 const app = express();
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
-const port = process.env.PORT || 5000;
+const productsRoute = require('./routes/products');
+const port = process.env.PORT || 3000;
+const connectDB = require('./db/connect');
 
 // middleware
 app.use(express.json())
@@ -15,7 +17,9 @@ app.get('/', (req, res)=>{
     <h1>Store API</h1>
     <a href="/api/v1/products">Products API</a>
 `)
-})
+});
+
+app.use('/api/v1/products', productsRoute);
 
 // products route
 
@@ -25,6 +29,7 @@ app.use(errorMiddleware);
 
 const start = (async()=>{
   try{
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, ()=>{
       console.log(`Server is listening on port ${port}...`)
     })
